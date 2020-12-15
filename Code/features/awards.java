d603a9db027a7b11b66f2b602c24bda2dbf7d55e
rahanmanoj <56351775+rahanmanoj@users.noreply.github.com>
2020-12-15 19:52:27 +0530

    PROJECT ON INVENTION MANAGEMENT SYSTEM

---
+++ Code/features/awards.java
@@ -0,0 +1,11 @@
+package features;
+
+public class awards {
+
+	public String award_id;
+	public String award_name;
+	public String award_category;
+	public String reward_amount;
+	public String sponsored_by;
+
+}
